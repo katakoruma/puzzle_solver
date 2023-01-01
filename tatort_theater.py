0179bb74cd274b58e4e518386104427b1981e3ab
katakoruma <stranix0209@gmail.com>
2023-01-01 22:06:47 +0100

    cleaned function

--- tatort_theater.py
+++ tatort_theater.py
@@ -3,26 +3,15 @@ import numpy as np
 import pandas as pd
 idx = pd.IndexSlice
 
-classes = { 
-            'Anordnung':    [1,2,3,4,5],
-            'Zimmer':       ['blau', 'rot', 'gruen', 'weiss', 'gelb'], 
-            'Name':         ['Joan', 'John', 'Martin', 'Mary', 'Marlon'], 
-            'Gegenstand':   ['Whiskeyflasche', 'Seil', 'Bueste', 'Schere', 'Leuchter'], 
-            'Besucher':     ['Platzanweiserin', 'Buehnenarbeiter', 'Regisseurin', 'Barkeeper', 'Maskenbildnerin'], 
-            'Uhrzeit':      ['03:00', '09:00', '12:00', '18:00', '21:00'],
-        }
+from tatort_conditions import *
 
 puz = puzzle(classes)
 
-index = (slice('blau', 'rot'), 'Joan', 'Seil', 'Regisseurin', '3:00')
-print(puz.cube.loc[idx[[],:,:,['Maskenbildnerin'],:]])
 
-
-import tatort_conditions as conditions
-
-puz.set(conditions.set, mode='set')
-#puz.set(conditions.unset, mode='unset')
+puz.set(a11.set, mode='set')
+#puz.set(a11.unset, mode='unset')
 
 
 puz.solve()
+puz.possible_combinations()
 puz.return_results()
